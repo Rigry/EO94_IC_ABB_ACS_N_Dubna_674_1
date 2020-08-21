b9137332be745cb4c3da6cea7bbcc7c10d1ce46b
Александр Александрович Голубев <aag@ganymed.factory.alexplus.ru>
2020-08-21 09:02:45 +0300

    swing in automatic

--- src/automatic.h
+++ src/automatic.h
@@ -1,46 +1,51 @@
 #pragma once
 
-template <class Horizontal, class Vertical, class Encoder>
+template <class Horizontal, class Vertical, class Swing, class Encoder>
 class Automatic
 {
    Horizontal& horizontal;
    Vertical  & vertical;
+   Swing     & swing;
    Encoder   & encoder;
    int16_t coordinate {0};
-   enum State {wait, horizontal_, vertical_} state {State::wait};
+   uint16_t qty_swings {0};
+   enum State {wait, horizontal_, vertical_, swing_} state {State::wait};
 public:
-   Automatic (Horizontal& horizontal, Vertical& vertical, Encoder& encoder);
+   Automatic (Horizontal& horizontal, Vertical& vertical, Swing& swing, Encoder& encoder);
    void move(int16_t coordinate);
    void move_up();
    void move_down();
+   void swing_start(uint16_t qty_swings);
    void stop();
    void reset();
    void operator()();
 };
 
-template <class Horizontal, class Vertical, class Encoder>
-Automatic <Horizontal, Vertical, Encoder>::Automatic (Horizontal& horizontal, Vertical& vertical, Encoder& encoder)
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+Automatic <Horizontal, Vertical, Swing, Encoder>::Automatic (Horizontal& horizontal, Vertical& vertical, Swing& swing, Encoder& encoder)
    : horizontal {horizontal}
    , vertical   {vertical}
+   , swing      {swing}
    , encoder    {encoder}
 {}
 
-template <class Horizontal, class Vertical, class Encoder>
-void Automatic <Horizontal, Vertical, Encoder>::stop()
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+void Automatic <Horizontal, Vertical, Swing, Encoder>::stop()
 {
    reset();
 }
 
-template <class Horizontal, class Vertical, class Encoder>
-void Automatic <Horizontal, Vertical, Encoder>::reset()
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+void Automatic <Horizontal, Vertical, Swing, Encoder>::reset()
 {
    horizontal.stop();
    vertical.stop();
+   swing.stop();
    state = State::wait;
 }
 
-template <class Horizontal, class Vertical, class Encoder>
-void Automatic <Horizontal, Vertical, Encoder>::move(int16_t coordinate)
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+void Automatic <Horizontal, Vertical, Swing, Encoder>::move(int16_t coordinate)
 {
    this->coordinate = coordinate;
    switch (state) {
@@ -55,11 +60,13 @@ void Automatic <Horizontal, Vertical, Encoder>::move(int16_t coordinate)
       break;
       case vertical_:
       break;
+      case swing_:
+      break;
    }
 }
 
-template <class Horizontal, class Vertical, class Encoder>
-void Automatic <Horizontal, Vertical, Encoder>::move_up()
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+void Automatic <Horizontal, Vertical, Swing, Encoder>::move_up()
 {
    switch(state) {
       case wait:
@@ -73,11 +80,13 @@ void Automatic <Horizontal, Vertical, Encoder>::move_up()
          if (not vertical.isUp())
             vertical.up();
       break;
+      case swing_:
+      break;
    }
 }
 
-template <class Horizontal, class Vertical, class Encoder>
-void Automatic <Horizontal, Vertical, Encoder>::move_down()
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+void Automatic <Horizontal, Vertical, Swing, Encoder>::move_down()
 {
    switch(state) {
       case wait:
@@ -91,11 +100,33 @@ void Automatic <Horizontal, Vertical, Encoder>::move_down()
          if (not vertical.isDown())
             vertical.down();
       break;
+      case swing_:
+      break;
    }
 }
 
-template <class Horizontal, class Vertical, class Encoder>
-void Automatic <Horizontal, Vertical, Encoder>::operator()()
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+void Automatic <Horizontal, Vertical, Swing, Encoder>::swing_start(uint16_t qty_swings)
+{
+   this->qty_swings = qty_swings;
+   switch(state) {
+      case wait:
+         if (not swing.is_done())
+            swing.start();
+         state = State::swing_;
+      break;
+      case horizontal_:
+      break;
+      case vertical_:
+      break;
+      case swing_:
+      break;
+   }
+
+}
+
+template <class Horizontal, class Vertical, class Swing, class Encoder>
+void Automatic <Horizontal, Vertical, Swing, Encoder>::operator()()
 {
    switch (state) {
       case wait:
@@ -108,6 +139,11 @@ void Automatic <Horizontal, Vertical, Encoder>::operator()()
       break;
       case vertical_:
          if (not vertical.isWorking() and (vertical.isUp() or vertical.isDown())){
+            state = State::wait;
+         }
+      break;
+      case swing_:
+         if (not swing.isWorking()){
             state = State::wait;
          }
       break;
